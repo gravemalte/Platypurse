1acbb00c1b2af60c507ddd528fa636750ec6ae4c
Daylien <malte.grave@uni-oldenburg.de>
2020-04-24 17:55:17 +0200

    First proposal

---
+++ app/Boot.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Boot.php
+|--------------------------------------------------------------------------
+|
+| This file serves as the startup building mechanism returns a Kernel instance
+|
+*/
+
+namespace Application;
+
+use Configuration;
