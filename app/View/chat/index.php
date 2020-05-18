883a38678ae6b059cc8200a84e013e11d56ba1b0
Daylien <malte.grave@uni-oldenburg.de>
2020-05-18 15:51:51 +0200

    Added first chat concept

---
+++ app/View/chat/index.php
@@ -0,0 +1,100 @@
+<div class="main-page">
+    <div class="container-chat clearfix">
+        <div class="people-list" id="people-list">
+            <div class="search">
+                <input type="text" placeholder="search"/>
+                <i class="fa fa-search"></i>
+            </div>
+            <ul class="list">
+                <li class="clearfix">
+                    <div class="about">
+                        <div class="name">Will Smith</div>
+                        <div class="status">
+                            <i class="fa fa-circle online"></i> online
+                        </div>
+                    </div>
+                </li>
+
+                <li class="clearfix">
+                    <div class="about">
+                        <div class="name">Jonson the Rock</div>
+                        <div class="status">
+                            <i class="fa fa-circle offline"></i> left 7 mins ago
+                        </div>
+                    </div>
+                </li>
+
+                <li class="clearfix">
+                    <div class="about">
+                        <div class="name">Tony Stark</div>
+                        <div class="status">
+                            <i class="fa fa-circle online"></i> online
+                        </div>
+                    </div>
+                </li>
+
+                <li class="clearfix">
+                    <div class="about">
+                        <div class="name">Hulk Hogan</div>
+                        <div class="status">
+                            <i class="fa fa-circle online"></i> online
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+        <div class="chat">
+            <div class="chat-header clearfix">
+
+                <div class="chat-about">
+                    <div class="chat-with">Chat with Will Smith</div>
+                </div>
+                <i class="fa fa-star"></i>
+            </div> <!-- end chat-header -->
+
+            <div class="chat-history">
+                <ul>
+                    <li class="chat-list">
+                        <div class="message-data">
+                            <span class="message-data-name"><i class="fa fa-circle online"></i> Will Smith</span>
+                            <span class="message-data-time">10:05 AM, Today</span>
+                        </div>
+                        <div class="message my-message">
+                            Hey how much is the blue platypus?
+                        </div>
+                    </li>
+
+                    <li class="clearfix chat-list">
+                        <div class="message-data align-right">
+                            <span class="message-data-time">10:14 AM, Today</span> &nbsp; &nbsp;
+                            <span class="message-data-name">Me</span> <i class="fa fa-circle me"></i>
+                        </div>
+                        <div class="message other-message float-right">
+                            Hey thanks for your request Will. The blue platypus costs 50$.
+                        </div>
+                    </li>
+
+                    <li class="chat-list">
+                        <div class="message-data">
+                            <span class="message-data-name"><i class="fa fa-circle online"></i> Will Smith</span>
+                            <span class="message-data-time">10:20 AM, Today</span>
+                        </div>
+                        <div class="message my-message">
+                            Thanks for your answer. I will take it!
+                        </div>
+                    </li>
+                </ul>
+
+            </div>
+
+            <div class="chat-message clearfix">
+                <textarea class="text-chat" name="message-to-send" id="message-to-send" placeholder="Type your message" rows="3"></textarea>
+                <i class="fa fa-file-o"></i> &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-file-image-o"></i>
+                <button class="button-send">Send</button>
+            </div>
+        </div>
+    </div>
+
+</div>
