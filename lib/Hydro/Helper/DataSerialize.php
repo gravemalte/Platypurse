53ebbe4893958ab2159a6b1aa06a7f3235f37cfa
Daylien <malte.grave@uni-oldenburg.de>
2020-05-28 20:00:59 +0200

    Fixed windows crash

--- lib/Hydro/Helper/DataSerialize.php
+++ lib/Hydro/Helper/DataSerialize.php
@@ -6,6 +6,14 @@ namespace Hydro\Helper;
 
 class DataSerialize
 {
+    /**
+     * Returns an array ob unserialize object.
+     * Cuts the last element out. See the in the FileWriter.php
+     * for more information.
+     *
+     * @param $dataModell
+     * @return array
+     */
     public static function unserializeData($dataModell){
         $dataArray = array();
         foreach ($dataModell as $data){
--- lib/Hydro/Helper/FileWriter.php
+++ lib/Hydro/Helper/FileWriter.php
@@ -5,6 +5,12 @@ namespace Hydro\Helper;
 class FileWriter
 {
 
+    /**
+     * Writes content to a file. Appending to it if it exists, if not
+     * the file will be created.
+     * @param $path
+     * @param $content
+     */
     public static function writeToFile($path, $content) {
         $fileStream = fopen($path, "a+");
         fwrite($fileStream, $content . "\n");
