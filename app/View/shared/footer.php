195eceeac8928ea063e89d2033dce71f055a3cbf
Piepmatz <derPiepmatz@users.noreply.github.com>
2020-05-31 23:16:50 +0200

    Updated Special Characters with Code Equivalents

--- app/View/shared/footer.php
+++ app/View/shared/footer.php
@@ -6,9 +6,9 @@
     </div>
     <div class="footer-copyright-container">
         <img src="assets/logo/svg/logo_1tone.svg" alt="footer-logo">
-        <p class="copyright">© 2020 Platypurse</p>
-        <p class="designed-for">Projekt für das Modul "Webprogrammierung" an der Carl von Ossietzky Universität Oldenburg</p>
-        <p class="designed-by">Gestaltet von <em>Malte Grave, Tim Hesse <wbr>& Marvin Kuhlmann</em></p>
+        <p class="copyright">&copy; 2020 Platypurse</p>
+        <p class="designed-for">Projekt für das Modul "Webprogrammierung" an der Carl&nbsp;von&nbsp;Ossietzky&nbsp;Universität&nbsp;Oldenburg</p>
+        <p class="designed-by">Gestaltet von <em>Malte&nbsp;Grave, Tim&nbsp;Hesse & Marvin&nbsp;Kuhlmann</em></p>
     </div>
 </footer>
 </body>
