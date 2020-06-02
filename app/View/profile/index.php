ef9a6d5fca741a8fcbefc338f975d97f2b6c6f2c
Daylien <malte.grave@uni-oldenburg.de>
2020-06-02 22:27:18 +0200

    Fixed unknown value in array

--- app/View/profile/index.php
+++ app/View/profile/index.php
@@ -7,7 +7,7 @@
                 <img src="assets/nav/user-circle-solid.svg" alt="profile image">
             </div>
             <div class="profile-displayname">
-                <p><?php echo $_SESSION['user-displayName']?></p>
+                <p><?php echo $_SESSION['user-display-name']?></p>
             </div>
             <div class="profile-rating">
                 <span class="fas fa-star checked"></span>
@@ -22,7 +22,7 @@
                         <p>Nachricht schreiben</p>
                     </div>
                 </a>
-                <?php if(!(isset($_SESSION['user-email']) == isset($_SESSION['user-email']))): ?>
+                <?php if(!(isset($_SESSION['user-ID']))): ?>
                 <a href="" class="button report-button">
                     <div>
                         <p>Nutzer melden</p>
