1b8c8f3b6b520258a0eff41681403b8c607fddc9
Daylien <malte.grave@uni-oldenburg.de>
2020-06-24 17:31:44 +0200

    Fixed search

--- app/View/search/index.php
+++ app/View/search/index.php
@@ -1,29 +1,30 @@
 <?php
-    use Controller\SearchController;
 
-    $searchText = $_GET['search'];
-    $sex = "";
-    $sexMaleSelected = "";
-    $sexFemaleSelected = "";
-    $age = array(0, 20);
-    $size = array(0, 75);
-    $weight = array(0, 3000);
+use Controller\SearchController;
 
-    if(isset($_GET['sex'])): $sex = $_GET['sex']; endif;
-    if(isset($_GET['sex'])):
-        $sex = $_GET['sex'];
-        if($sex == "männlich"): $sexMaleSelected = "selected"; endif;
-        if($sex == "weiblich"): $sexFemaleSelected = "selected"; endif;
-    endif;
-    if(isset($_GET['age'])):
-        $age = $_GET['age'];
-    endif;
-    if(isset($_GET['size'])):
-        $size = $_GET['size'];
-    endif;
-    if(isset($_GET['weight'])):
-        $weight = $_GET['weight'];
-    endif;?>
+$searchText = $_GET['search'];
+$sex = "";
+$sexMaleSelected = "";
+$sexFemaleSelected = "";
+$age = array(0, 20);
+$size = array(0, 75);
+$weight = array(0, 3000);
+
+if (isset($_GET['sex'])): $sex = $_GET['sex']; endif;
+if (isset($_GET['sex'])):
+    $sex = $_GET['sex'];
+    if ($sex == "männlich"): $sexMaleSelected = "selected"; endif;
+    if ($sex == "weiblich"): $sexFemaleSelected = "selected"; endif;
+endif;
+if (isset($_GET['age'])):
+    $age = $_GET['age'];
+endif;
+if (isset($_GET['size'])):
+    $size = $_GET['size'];
+endif;
+if (isset($_GET['weight'])):
+    $weight = $_GET['weight'];
+endif; ?>
 <main class="main-page filter-page">
     <div class="filter-area">
         <div class="filter-container card">
@@ -56,9 +57,11 @@
                         </div>
                         <div class="multi-thumb-slider-container" role="group" aria-labelledby="multi-thumb-slider">
                             <label for="filter-age-range-1" hidden></label>
-                            <input type="range" min="0" max="20" value="<?php echo min($age) ?>" id="filter-age-range-1" name="age[]">
+                            <input type="range" min="0" max="20" value="<?php echo min($age) ?>" id="filter-age-range-1"
+                                   name="age[]">
                             <label for="filter-age-range-2" hidden></label>
-                            <input type="range" min="0" max="20" value="<?php echo max($age) ?>" id="filter-age-range-2" name="age[]">
+                            <input type="range" min="0" max="20" value="<?php echo max($age) ?>" id="filter-age-range-2"
+                                   name="age[]">
                         </div>
                     </div>
                     <div>
@@ -69,9 +72,11 @@
                         </div>
                         <div class="multi-thumb-slider-container" role="group" aria-labelledby="multi-thumb-slider">
                             <label for="filter-size-range-1" hidden></label>
-                            <input type="range" min="0" max="75" value="<?php echo min($size) ?>" id="filter-size-range-1" name="size[]">
+                            <input type="range" min="0" max="75" value="<?php echo min($size) ?>"
+                                   id="filter-size-range-1" name="size[]">
                             <label for="filter-size-range-2" hidden></label>
-                            <input type="range" min="0" max="75" value="<?php echo max($size) ?>" id="filter-size-range-2" name="size[]">
+                            <input type="range" min="0" max="75" value="<?php echo max($size) ?>"
+                                   id="filter-size-range-2" name="size[]">
                         </div>
                     </div>
                     <div>
@@ -82,9 +87,11 @@
                         </div>
                         <div class="multi-thumb-slider-container" role="group" aria-labelledby="multi-thumb-slider">
                             <label for="filter-weight-range-1" hidden></label>
-                            <input type="range" min="0" max="3000" value="<?php echo min($weight) ?>" id="filter-weight-range-1" name="weight[]">
+                            <input type="range" min="0" max="3000" value="<?php echo min($weight) ?>"
+                                   id="filter-weight-range-1" name="weight[]">
                             <label for="filter-weight-range-2" hidden></label>
-                            <input type="range" min="0" max="3000" value="<?php echo max($weight) ?>" id="filter-weight-range-2" name="weight[]">
+                            <input type="range" min="0" max="3000" value="<?php echo max($weight) ?>"
+                                   id="filter-weight-range-2" name="weight[]">
                         </div>
                     </div>
                     <div class="filter-button-container">
@@ -107,35 +114,24 @@
         <div class="search-results-container">
             <?php
             $offers = SearchController::getOffers($searchText, $sex, $age, $size, $weight);
-            if(!empty($offers)):?>
+            if (!empty($offers)): ?>
             <div class="offer-list-container">
-                <?php
-                $offers = SearchController::getOffers($searchText, $sex, $age, $size, $weight);
-                if(!empty($offers)):?>
-                <div class="offer-list-container">
-                    <?php foreach($offers as $offer): ?>
-                        <a class="offer-list-link" href="offer?id=<?= $offer->getId();?>">
-                            <div class="offer-list-item card">
-                                <img src="<?= $offer->getPictureOnPosition(0); ?>" alt="">
-                                <p class="name"><?= $offer->getPlatypus()->getName();?></p>
-                                <p class="description"><?= $offer->getDescription();?></p>
-                                <div class="price-tag-container">
-                                    <p class="price-tag"><?= $offer->getPrice();?></p>
-                                </div>
+                <?php foreach ($offers as $offer): ?>
+                    <a class="offer-list-link" href="offer?id=<?= $offer->getId(); ?>">
+                        <div class="offer-list-item card">
+                            <img src="<?= $offer->getPictureOnPosition(0); ?>" alt="">
+                            <p class="name"><?= $offer->getPlatypus()->getName(); ?></p>
+                            <p class="description"><?= $offer->getDescription(); ?></p>
+                            <div class="price-tag-container">
+                                <p class="price-tag"><?= $offer->getPrice(); ?></p>
                             </div>
-                        </a>
-                    <?php endforeach;
-                    else: ?>
-                        <div>
-                            <h1>Sorry, es gibt leider keine passenden Angebote. ¯\_(ツ)_/¯</h1>
                         </div>
-                    </div>
-                </a>
+                    </a>
                 <?php endforeach;
                 else: ?>
-                <div>
-                    <h1>Sorry, es gibt leider keine passenden Angebote. ¯\_(ツ)_/¯</h1>
-                </div>
+                    <div>
+                        <h1>Sorry, es gibt leider keine passenden Angebote. ¯\_(ツ)_/¯</h1>
+                    </div>
                 <?php endif; ?>
             </div>
         </div>
