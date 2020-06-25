f3f501db234ec8979e0d980a5b05a885b20049c3
Ceitcher <42640394+Ceitcher@users.noreply.github.com>
2020-06-25 20:50:54 +0200

    Search string should now search for matches in platypus name and offer description
    Is not really tested since I wrote the update online on github. Works basically only on faith in my skills to write proper SQL statements.

--- app/Controller/SearchController.php
+++ app/Controller/SearchController.php
@@ -21,12 +21,13 @@ class SearchController extends BaseController
 
 
     public static function getOffers($like = "", $sex = "", $age = array(0, 20), $size = array(0, 20), $weight = array(0, 3000)) {
-        $whereClause = "INNER JOIN platypus ON platypus.p_id = offer.p_id WHERE " .COLUMNS_PLATYPUS['name']. " LIKE ? 
+        $whereClause = "INNER JOIN platypus ON platypus.p_id = offer.p_id WHERE (" .COLUMNS_PLATYPUS['name']. " LIKE ?
+        OR " .COLUMNS_OFFER['description']. " LIKE ?)
         AND ".COLUMNS_PLATYPUS['age_years']." BETWEEN ? and ?
         AND ".COLUMNS_PLATYPUS['size']." BETWEEN ? and ?
         AND ".COLUMNS_PLATYPUS['weight']." BETWEEN ? and ?
         AND ".TABLE_OFFER.".".COLUMNS_OFFER['active']." = 1";
-        $values = array("%" .$like. "%", min($age), max($age), min($size), max($size), min($weight), max($weight));
+        $values = array("%" .$like. "%", "%" .$like. "%", min($age), max($age), min($size), max($size), min($weight), max($weight));
 
         if(!empty($sex)):
             $whereClause .= " AND ".COLUMNS_PLATYPUS['sex']. " = ?";
