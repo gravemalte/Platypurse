30697c026a40a5c95f1cf3989734d72663a5eed2
Piepmatz <derPiepmatz@users.noreply.github.com>
2020-07-06 17:34:29 +0200

    Added Leaflet to use later for a Map

--- app/View/offer/header.php
+++ app/View/offer/header.php
@@ -4,5 +4,17 @@ use Hydro\Helper\CacheBuster;
 
     <!-- elements per page -->
     <link rel="stylesheet" href="<?= CacheBuster::serve("css/offer-page.css") ?>">
+    <link
+            rel="stylesheet"
+            href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+            integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+            crossorigin=""
+    />
+    <script
+            src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+            integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+            crossorigin=""
+    ></script>
+    <script type="text/javascript" src="<?= CacheBuster::serve("js/map.js") ?>"></script>
 
 </head>
