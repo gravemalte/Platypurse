5ad269ec2eda56994ec90802b9dbad2d5905b94e
Piepmatz <derPiepmatz@users.noreply.github.com>
2020-07-06 11:53:06 +0200

    Added Documentation to the Chat Client

--- public/js/modules/ChatThreadMap.js
+++ public/js/modules/ChatThreadMap.js
@@ -1,11 +1,23 @@
 "use strict";
 
+// function to load the ChatThreadMap module async
 function buildChatThreadMap(modules) {
+
+    // modules used by this module
     const ChatThread = modules.ChatThread;
 
     class ChatThreadMap extends Map {
+        /**
+         * Maps all chat threads into one map with some extras.
+         */
         constructor() {
             super();
+
+            /**
+             * The HTML element that displays the map.
+             *
+             * @type {HTMLElement}
+             */
             this.container = document.getElementById("chat-list-container");
         }
     }
