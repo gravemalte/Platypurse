6637ac94d81b8c9ff6837dc2b2c155f4778b2e29
Ceitcher <42640394+Ceitcher@users.noreply.github.com>
2020-07-15 17:36:42 +0200

    Fixed typo

--- app/View/error/subPageNotFound.php
+++ app/View/error/subPageNotFound.php
@@ -1,7 +1,7 @@
 <main class="main-page">
     <div class="main-area">
         <div>
-            <h1>Sorry, aber dies Unterseite <b><i><?php echo $_GET['url'] ?> </i></b> existiert nicht ¯\_(ツ)_/¯</h1>
+            <h1>Sorry, aber diese Unterseite <b><i><?php echo $_GET['url'] ?> </i></b> existiert nicht. ¯\_(ツ)_/¯</h1>
         </div>
         <div>
             <h2>Klicke <a href="/">hier</a> um zur Startseite zurückzukehren</h2>
