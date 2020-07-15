6ae3d72ee43b8bb705c310a9bc8d72383b2ee0a0
Ceitcher <42640394+Ceitcher@users.noreply.github.com>
2020-07-15 17:37:27 +0200

    Fixed typo

--- app/View/error/databaseError.php
+++ app/View/error/databaseError.php
@@ -1,7 +1,7 @@
 <main class="main-page">
     <div class="main-area">
         <div>
-            <h1>Sorry, aber unsere Datenbank hat einen fehler festgestellt. Bitte versuche es erneut ¯\_(ツ)_/¯</h1>
+            <h1>Sorry, aber die Datenbank hat einen Fehler festgestellt. Bitte versuche es erneut. ¯\_(ツ)_/¯</h1>
         </div>
         <div>
             <h2>Klicke <a href="/">hier</a> um zur Startseite zurückzukehren</h2>
