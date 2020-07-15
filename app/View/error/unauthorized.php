970c13321ed24e4c47455e501a9ecd527a597e04
Ceitcher <42640394+Ceitcher@users.noreply.github.com>
2020-07-15 17:36:18 +0200

    Update formulation

--- app/View/error/unauthorized.php
+++ app/View/error/unauthorized.php
@@ -1,7 +1,7 @@
 <main class="main-page">
     <div class="main-area">
         <div>
-            <h1>Tut uns leid, aber du bist nicht authorizierd dazu. ¯\_(ツ)_/¯</h1>
+            <h1>Tut uns leid, aber dafür hast du keine Berechtigung. ¯\_(ツ)_/¯</h1>
         </div>
         <div>
             <h2>Bitte melde dich erneut an und versuche es erneut. <a href="/login">Login.</a></h2>
