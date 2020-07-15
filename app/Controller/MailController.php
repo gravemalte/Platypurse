5b58c567ea566d870593d878bac4318f71130210
Piepmatz <derPiepmatz@users.noreply.github.com>
2020-07-15 09:41:38 +0200

    Started to work in the Controller

--- app/Controller/MailController.php
+++ app/Controller/MailController.php
@@ -3,13 +3,42 @@
 
 namespace Controller;
 
+use \Model\DAO\MailDAO;
+use \Hydro\Base\Database\Driver\SQLite;
+use \Model\MailModel;
+
 
 class MailController
 {
 
     public function index() {
+
+        if (!isset($_GET['id'])) {
+            header('location: ' . URL . 'error');
+            exit();
+        }
+
         // load views
         require APP . 'View/mail/header.php';
         require APP . 'View/mail/index.php';
+    }
+
+    public static function getMailContent() {
+        if (!isset($_GET['id'])) {
+            http_response_code(400);
+            header('location: ' . URL . 'error');
+            exit();
+        }
+
+        $dao = new MailDAO(SQLite::connectToSQLite());
+        $mail = MailModel::getFromDatabase($dao, $_GET['id']);
+
+        if (!$mail->exists()) {
+            http_response_code(404);
+            header('location: ' . URL . 'error');
+            exit();
+        }
+
+        echo $mail->getContent();
     }
 }
