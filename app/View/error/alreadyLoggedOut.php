b0d682fa4404fad510fa3d85d9715aa0038f5adc
Ceitcher <42640394+Ceitcher@users.noreply.github.com>
2020-07-15 17:39:39 +0200

    Update formulation

--- app/View/error/alreadyLoggedOut.php
+++ app/View/error/alreadyLoggedOut.php
@@ -1,7 +1,7 @@
 <main class="main-page">
     <div class="main-area">
         <div>
-            <h1>Tut uns leid, aber du bist abgemeldet ¯\_(ツ)_/¯</h1>
+            <h1>Tut uns leid, aber du bist nicht angemeldet. ¯\_(ツ)_/¯</h1>
         </div>
         <div>
             <h2>Klicke <a href="/login">hier</a> um dich wieder anzumelden</h2>
