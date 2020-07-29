cae2ff7d2f64202aa70198acc147e23146644076
Piepmatz <derPiepmatz@users.noreply.github.com>
2020-07-29 14:46:10 +0200

    Documented a bit the rate-user script

--- public/js/rate-user.js
+++ public/js/rate-user.js
@@ -1,8 +1,12 @@
 "use strict";
 
+// allow users to rate other users
 (function() {
     let ratingButtons = new Map();
 
+    /**
+     * Apply for every rating button an event.
+     */
     function addButtonRatingEvent() {
         ratingButtons.set(1, document.getElementById("user-rating-1"));
         ratingButtons.set(2, document.getElementById("user-rating-2"));
@@ -12,40 +16,46 @@
 
         ratingButtons.forEach((value, key, map) => {
             let handler = buildRate(key);
-            value.addEventListener("click", event => handler(event));
+            value.addEventListener("click", event => handler());
         });
     }
 
+    // add button event on first load
     window.addEventListener("DOMContentLoaded", event => addButtonRatingEvent());
 
+    /**
+     * Builds the rating handler
+     * @param {number} rating
+     * @returns {function()}
+     */
     function buildRate(rating) {
-        return async function(event) {
+        return async function() {
+
             let payload = new FormData();
-            payload.set("rating", rating);
+            payload.set("rating", rating.toString());
             payload.set("csrf", document.getElementById("csrf-token").value);
             payload.set("rating-user-id", document.getElementById("rating-user-id").value);
-            console.log(payload);
+
             let sendMessageResponse = await fetch("./profile/rateUser", {
                 method: "POST",
                 body: payload
             });
-            console.log(sendMessageResponse);
-            console.log(await sendMessageResponse.text());
             if (!sendMessageResponse.ok) return;
 
+            // after rate was successful update buttons
             let pageUpdateResponse = await fetch(window.location, {
                 method: "GET"
             });
-            console.log(pageUpdateResponse);
+
             if (!pageUpdateResponse.ok) return;
             let pageUpdateText = await pageUpdateResponse.text();
             let parser = new DOMParser();
             let pageUpdateDocument = parser.parseFromString(pageUpdateText, 'text/html');
-            console.log(pageUpdateDocument);
+
             document.getElementById("user-rating").innerHTML
                 = pageUpdateDocument.getElementById("user-rating").innerHTML;
-            console.log("done");
 
+            // add button event after the new pages are loaded
             addButtonRatingEvent();
         }
     }
