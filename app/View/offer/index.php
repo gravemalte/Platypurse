ed626bec9a4adb53955f59766cb094189daba825
Ceitcher <marvin.kuhlmann@ewetel.net>
2020-07-29 14:02:30 +0200

    Fixed invalid zip handling

--- app/View/offer/index.php
+++ app/View/offer/index.php
@@ -10,10 +10,15 @@ OfferController::offerClickPlusOne($offer);
 $seller = $offer->getUser();
 $zipcode = $offer->getZipcode();
 $zipcoordinates = $offer->getZipCoordinates();
+$invalidZipName = "Unbekannt";
 if($zipcoordinates):
     $location = $zipcoordinates->getName();
     $map_lat = $zipcoordinates->getLat();
     $map_lon = $zipcoordinates->getLon();
+else:
+    $location = $invalidZipName;
+    $map_lat = 0;
+    $map_lon = 0;
 endif;
 $isSaved = false;
 
@@ -42,7 +47,7 @@ if(isset($_SESSION['currentUser'])) {
                     <div class="price-tag">
                         <p><?=$offer->getShortPrice();?></p>
                         <?php if($offer->getNegotiable()): ?>
-                        <span>VB</span>
+                            <span>VB</span>
                         <?php endif; ?>
                     </div>
                 </div>
@@ -55,21 +60,21 @@ if(isset($_SESSION['currentUser'])) {
                         </div>
                     </a>
                     <?php if($isSaved):?>
-                    <form action="offer/removeFromSavedList" method="post">
-                        <input type="text" id="save-id" name="offerId" hidden value="<?=$offer->getId();?>">
-                        <label for="save-id" hidden>Speicher-ID</label>
-                        <button class="save-offer-button button">
-                            <span>Von der Merkliste entfernen</span>
-                        </button>
-                    </form>
+                        <form action="offer/removeFromSavedList" method="post">
+                            <input type="text" id="save-id" name="offerId" hidden value="<?=$offer->getId();?>">
+                            <label for="save-id" hidden>Speicher-ID</label>
+                            <button class="save-offer-button button">
+                                <span>Von der Merkliste entfernen</span>
+                            </button>
+                        </form>
                     <?php else: ?>
-                    <form action="offer/offerToSavedList" method="post">
-                        <input type="text" id="save-id" name="offerId" hidden value="<?=$offer->getId();?>">
-                        <label for="save-id" hidden>Speicher-ID</label>
-                        <button class="save-offer-button button">
-                            <span>Zur Merkliste</span>
-                        </button>
-                    </form>
+                        <form action="offer/offerToSavedList" method="post">
+                            <input type="text" id="save-id" name="offerId" hidden value="<?=$offer->getId();?>">
+                            <label for="save-id" hidden>Speicher-ID</label>
+                            <button class="save-offer-button button">
+                                <span>Zur Merkliste</span>
+                            </button>
+                        </form>
                     <?php endif; ?>
                 </div>
                 <div class="profile-container card">
@@ -180,52 +185,54 @@ if(isset($_SESSION['currentUser'])) {
                                 <?= $offer->getCreateDate() ?> UTC
                             </strong></p>
                         <?php if(!empty($offer->getEditDate())): ?>
-                        <p>Zuletzt bearbeitet: <strong class="date-display">
-                                <?= $offer->getEditDate() ?> UTC</strong></p>
+                            <p>Zuletzt bearbeitet: <strong class="date-display">
+                                    <?= $offer->getEditDate() ?> UTC</strong></p>
                         <?php endif; ?>
                     </div>
                 </div>
                 <div class="offer-interactions-container">
                     <?php if((isset($_SESSION['currentUser']))): ?>
-                    <form action="report">
-                        <input type="text" id="report-id" name="id" hidden value="<?=$offer->getId();?>">
-                        <label for="report-id" hidden>Änderungs-ID</label>
-                        <button id="submit-report" class="delete-submit button" type="submit" hidden></button>
-                        <label for="submit-report" hidden>Artikel melden</label>
-                        <label for="submit-report" class="fas fa-exclamation-triangle" title="Artikel melden"></label>
-                    </form>
-                    <?php if($_SESSION["currentUser"]->getId() == $seller->getId() || $_SESSION["currentUser"]->isAdmin()): ?>
-                    <form action="offer/delete" method="post" data-needs-confirmation>
-                        <input type="text" id="delete-platypus-id" name="platypusId" hidden value="<?=$offer->getPlatypus()->getId();?>">
-                        <input type="text" id="delete-offer-id" name="offerId" hidden value="<?=$offer->getId();?>">
-                        <label for="delete-id" hidden>Änderungs-ID</label>
-                        <button id="submit-delete" class="delete-button button" type="submit" hidden></button>
-                        <label for="submit-delete" hidden>Artikel löschen</label>
-                        <label for="submit-delete" class="fas fa-trash-alt" title="Artikel löschen"></label>
-                    </form>
-                    <form action="create">
-                        <input type="text" id="create-id" name="id" hidden value="<?=$offer->getId();?>">
-                        <label for="create-id" hidden>Änderungs-ID</label>
-                        <button id="submit-create" class="create-button button" type="submit" hidden></button>
-                        <label for="submit-create" hidden>Artikel anpassen</label>
-                        <label for="submit-create" class="fas fa-pencil-alt" title="Artikel anpassen"></label>
-                    </form>
-                    <?php endif; ?>
+                        <form action="report">
+                            <input type="text" id="report-id" name="id" hidden value="<?=$offer->getId();?>">
+                            <label for="report-id" hidden>Änderungs-ID</label>
+                            <button id="submit-report" class="delete-submit button" type="submit" hidden></button>
+                            <label for="submit-report" hidden>Artikel melden</label>
+                            <label for="submit-report" class="fas fa-exclamation-triangle" title="Artikel melden"></label>
+                        </form>
+                        <?php if($_SESSION["currentUser"]->getId() == $seller->getId() || $_SESSION["currentUser"]->isAdmin()): ?>
+                            <form action="offer/delete" method="post" data-needs-confirmation>
+                                <input type="text" id="delete-platypus-id" name="platypusId" hidden value="<?=$offer->getPlatypus()->getId();?>">
+                                <input type="text" id="delete-offer-id" name="offerId" hidden value="<?=$offer->getId();?>">
+                                <label for="delete-id" hidden>Änderungs-ID</label>
+                                <button id="submit-delete" class="delete-button button" type="submit" hidden></button>
+                                <label for="submit-delete" hidden>Artikel löschen</label>
+                                <label for="submit-delete" class="fas fa-trash-alt" title="Artikel löschen"></label>
+                            </form>
+                            <form action="create">
+                                <input type="text" id="create-id" name="id" hidden value="<?=$offer->getId();?>">
+                                <label for="create-id" hidden>Änderungs-ID</label>
+                                <button id="submit-create" class="create-button button" type="submit" hidden></button>
+                                <label for="submit-create" hidden>Artikel anpassen</label>
+                                <label for="submit-create" class="fas fa-pencil-alt" title="Artikel anpassen"></label>
+                            </form>
+                        <?php endif; ?>
                     <?php endif; ?>
                 </div>
             </div>
         </div>
         <?php if (!(is_null($zipcode) || $zipcode === "")): ?>
-        <div class="map-container card no-js-hide">
-            <input type="hidden" id="map-lat" value="<?= $map_lat ?>">
-            <input type="hidden" id="map-lon" value="<?= $map_lon ?>">
-            <div class="map-text-container">
-                <h3>Standort:</h3>
-                <p><?= $zipcode ?></p>
-                <p><?= $location ?></p>
+            <div class="map-container card no-js-hide">
+                <input type="hidden" id="map-lat" value="<?= $map_lat ?>">
+                <input type="hidden" id="map-lon" value="<?= $map_lon ?>">
+                <div class="map-text-container">
+                    <h3>Standort:</h3>
+                    <p><?= $zipcode ?></p>
+                    <p><?= $location ?></p>
+                </div>
+                <?php if($location != $invalidZipName):?>
+                    <div id="map"></div>
+                <?php endif;?>
             </div>
-            <div id="map"></div>
-        </div>
         <?php endif; ?>
     </div>
     <div class="confirm-changes-container-background" id="confirm-changes-container" hidden>
