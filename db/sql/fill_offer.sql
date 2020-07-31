037e0ef583330431e4fdf8e027464d8f6cc13b03
Ceitcher <marvin.kuhlmann@ewetel.net>
2020-07-31 19:44:58 +0200

    Fill offers now with random values

--- db/sql/fill_offer.sql
+++ db/sql/fill_offer.sql
@@ -1,23 +1,23 @@
 INSERT INTO platypus (name, age_years, sex, size, weight)
 VALUES
-("Perry", 7, "männlich", 47, 1350),
-("Schnabella", 5, "weiblich", 35, 999),
-("Schnabello", 9, "männlich", 11, 1111),
-("Daisy", 4, "weiblich", 39, 1292),
-("Flap", 17, "männlich", 46, 1437),
-("Anatina", 3, "weiblich", 40, 1000),
-("Rufus", 4, "männlich", 44, 1200),
-("Goal", 2, "weiblich", 36, 800),
-("Bozo", 6, "männlich", 57, 3000);
+("Perry", ABS(RANDOM()) % 21, "männlich", ABS(RANDOM()) % 76, ABS(RANDOM()) % 3001),
+("Schnabella", ABS(RANDOM()) % 21, "weiblich", ABS(RANDOM()) % 76, ABS(RANDOM()) % 3001),
+("Schnabello", ABS(RANDOM()) % 21, "männlich", ABS(RANDOM()) % 76, ABS(RANDOM()) % 3001),
+("Daisy", ABS(RANDOM()) % 21, "weiblich", ABS(RANDOM()) % 76, ABS(RANDOM()) % 3001),
+("Flap", ABS(RANDOM()) % 21, "männlich", ABS(RANDOM()) % 76, ABS(RANDOM()) % 3001),
+("Anatina", ABS(RANDOM()) % 21, "weiblich", ABS(RANDOM()) % 76, ABS(RANDOM()) % 3001),
+("Rufus", ABS(RANDOM()) % 21, "männlich", ABS(RANDOM()) % 76, ABS(RANDOM()) % 3001),
+("Goal", ABS(RANDOM()) % 21, "weiblich", ABS(RANDOM()) % 76, ABS(RANDOM()) % 3001),
+("Bozo", ABS(RANDOM()) % 21, "männlich", ABS(RANDOM()) % 76, ABS(RANDOM()) % 3001);
 
 INSERT INTO offer (u_id, p_id, price, negotiable, description, zipcode)
 VALUES
-(3, 1, 12999, 0, "Er ist ein Schnabeltier, die machen nicht viel. Ist aber tagsüber immer verschwunden...", "26129"),
-(3, 2, 9900, 0, "Wärst du so freundlich und kaufst dieses Schnabeltier?", "26180"),
-(3, 3, 9900, 1, "Rietlebanchs sesied fauk!", "01108"),
-(3, 4, 13300, 1, "", "20457"),
-(3, 5, 7950, 0, "Ist ein älteres Tier und hat Erinnerungen hinterlassen.", null),
-(3, 6, 150000, 1, "Selbsterklärend.", "26121"),
-(3, 7, 550, 0, "Bisschen vertrottelt, besitzt aber Erfindergeist!", "26169"),
-(3, 8, 11879, 0, "Gutmütiges Vieh. Hat aber gespaltene Persönlichkeiten", "26655"),
-(3, 9, 10000, 1, "In Ehrfurcht vor der Größe dieses Jungens. Absolute Einheit.", "49661");
+(3, 1, ABS(RANDOM()) % 100001, ABS(RANDOM()) % 2, "Er ist ein Schnabeltier, die machen nicht viel. Ist aber tagsüber immer verschwunden...", "26129"),
+(3, 2, ABS(RANDOM()) % 100001, ABS(RANDOM()) % 2, "Wärst du so freundlich und kaufst dieses Schnabeltier?", "26180"),
+(3, 3, ABS(RANDOM()) % 100001, ABS(RANDOM()) % 2, "Rietlebanchs sesied fauk!", "01108"),
+(3, 4, ABS(RANDOM()) % 100001, ABS(RANDOM()) % 2, "", "20457"),
+(3, 5, ABS(RANDOM()) % 100001, ABS(RANDOM()) % 2, "Ist ein älteres Tier und hat Erinnerungen hinterlassen.", null),
+(3, 6, ABS(RANDOM()) % 100001, ABS(RANDOM()) % 2, "Selbsterklärend.", "26121"),
+(3, 7, ABS(RANDOM()) % 100001, ABS(RANDOM()) % 2, "Bisschen vertrottelt, besitzt aber Erfindergeist!", "26169"),
+(3, 8, ABS(RANDOM()) % 100001, ABS(RANDOM()) % 2, "Gutmütiges Vieh. Hat aber gespaltene Persönlichkeiten", "26655"),
+(3, 9, ABS(RANDOM()) % 100001, ABS(RANDOM()) % 2, "In Ehrfurcht vor der Größe dieses Jungens. Absolute Einheit.", "49661");
